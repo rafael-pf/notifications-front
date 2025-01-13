830653d45e5168d6aa6b543f91490504266fc648
Rafael <rpf@cin.ufpe.br>
2025-01-13 13:11:17 -0300

    lint fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
 import Image from 'next/image';
-import { redirect } from 'next/navigation';
 
 import { useNotification } from 'contexts/NotificationContext';
 
